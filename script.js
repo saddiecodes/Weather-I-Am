749140b065630b2d6046001afab9190b106459b3
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-29 18:02:56 +0000

    adding java script elements

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Replace with your API key
+    const apiKey = "YOUR_API_KEY";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/";
+    const searchForm = $("#search-form");
+    const searchInput = $("#search-input");
+    const historyList = $("#history"); 
+    
+}); 
