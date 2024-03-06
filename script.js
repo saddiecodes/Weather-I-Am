b408400ce909fa03d620013fc4d1c48e49f1123a
saddiecodes <hannahbarrie1999@gmail.com>
2024-03-06 12:26:04 +0000

    displayed weather forecast

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Define global variables
+const apiKey = "ee3ada3a53770308ef9a35a3b65b0744"; 
+const apiUrl = "https://api.openweathermap.org/data/2.5/";
+const searchForm = $("#search-form");
+const searchInput = $("#search-input");
+const historyList = $("#history");
 $(document).ready(function () {
     const apiKey = "ee3ada3a53770308ef9a35a3b65b0744";
     const apiUrl = "https://api.openweathermap.org/data/2.5/";
@@ -35,7 +41,7 @@ $(document).ready(function () {
       });
     }
     function weatherData(lat, lon) {
-    const weatherDataUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; 
+    const weatherDataUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`; 
     $.ajax({
       url: weatherDataUrl,
       method: "GET",
@@ -53,8 +59,43 @@ $(document).ready(function () {
       let cityTitle = document.createElement("h1")
       cityTitle.textContent = data.city.name 
       todaysWeatherElement.appendChild(cityTitle)
+      //Wind speed 
+      let windSpeed = document.createElement("h1")
+      windSpeed.textContent = data.list[0].wind.speed
+      todaysWeatherElement.appendChild(windSpeed) 
+      //Humditiy
+      let humidityShow = document.createElement("h1")
+      humidityShow.textContent = data.list[0].main.humidity
+      todaysWeatherElement.appendChild(humidityShow)
+      //Temperature
+      let tempNow = document.createElement("h1")
+      tempNow.textContent = data.list[0].main.temp
+      todaysWeatherElement.appendChild(tempNow)
+      //Date
+      let dateNow = document.createElement("h1")
+      dateNow.textContent = data.list[0].dt_txt
+      todaysWeatherElement.appendChild(dateNow)
+      displayFutureWeather(data)
     }
-    //current wind, humidity 
+  // Future weather
+  function displayFutureWeather (data) {
+    console.log(data)
+    var days=[3,10,17,24,31] 
+    for (let index = 0; index < days.length; index++) {
+      const element = days[index];
+      //Wind Speed
+      let windSpeed = document.createElement("h1")
+      windSpeed.textContent = data.list[element].wind.speed
+      todaysWeatherElement.appendChild(windSpeed) 
+      //Add other elements to show on page
+      
+    }
+
+  }
   });
+// Set global variables, including Open Weather Maps API Key
+
+
+
      
       
